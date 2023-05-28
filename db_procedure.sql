62e468febcff99b1359a67f81e3249832fc23940
Davidbraga6 <davidjosebraga@gmail.com>
2023-05-29 00:41:52 +0100

    add: sp verifyResultType

---
+++ db_procedure.sql
@@ -0,0 +1,31 @@
+-- Store Procedure que verifica se tipo de informação existe recebe uma string (tipo de estatística) e @resultMessage = @message OUTPUT
+CREATE PROCEDURE VerifyResultType
+    @type VARCHAR(100),
+    @resultMessage VARCHAR(200) OUTPUT
+AS
+BEGIN
+
+    DECLARE @typeExists BIT;
+
+    -- Verificar se o tipo de estatística existe
+    SELECT @typeExists = CASE WHEN EXISTS (
+        SELECT 1
+        FROM dbo.IndexType
+        WHERE title = @type
+    ) THEN 1 ELSE 0 END;
+
+    IF @typeExists = 1
+    BEGIN
+        SET @resultMessage = 'Tipo de estatística encontrado.';
+    END
+    ELSE
+    BEGIN
+        SET @resultMessage = 'Tipo de estatística inexistente.';
+    END;
+END;
+GO
+
+
+DECLARE @message VARCHAR(200);
+EXEC VerifyResultType 'Disciplina com mais alunos', @resultMessage = @message OUTPUT;
+SELECT @message;
