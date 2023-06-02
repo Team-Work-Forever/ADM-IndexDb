730bfeba3eec320ca150ab1b9f58abe3ea4c71ed
CavasCallahan <dassuncao1234@gmail.com>
2023-06-02 14:50:22 +0100

    add: FileStream Configuration

--- file_stream/create_file_stream_table.sql
+++ file_stream/create_file_stream_table.sql
@@ -1,3 +1,21 @@
+EXEC sp_configure filestream_access_level, 2
+RECONFIGURE
+
+DBCC TRACEON(12324, -1)
+
+alter database db_index
+add filegroup FileStreamDBFS contains filestream;
+go
+
+alter database db_index
+add file
+  ( NAME = N'FileStreamDBFS', FILENAME = N'C:\tp_adm\DBFilestream\FileStreamDBFS' , MAXSIZE = UNLIMITED )
+to filegroup FileStreamDBFS;
+go
+
+USE [db_index]
+GO
+
 CREATE TABLE [dbo].FileStreamTable
 (
     [FSID] UNIQUEIDENTIFIER ROWGUIDCOL NOT NULL UNIQUE,
