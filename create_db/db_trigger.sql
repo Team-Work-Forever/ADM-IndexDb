07d2c69ac0625d46d5bd80f2f3e175fd5382f0b3
Davidbraga6 <davidjosebraga@gmail.com>
2023-06-02 21:41:40 +0100

    fix: Validation trigger

--- create_db/db_trigger.sql
+++ create_db/db_trigger.sql
@@ -1,35 +1,16 @@
 -- Trigger que verifica se o código do curso existe na tabela Course e se não cancela insert da Class
-CREATE TRIGGER ValidateClassInsert
+CREATE OR ALTER TRIGGER ValidateClassInsert
 ON dbo.Class
 AFTER INSERT
 AS
 BEGIN
-
-    IF EXISTS (
+    IF NOT EXISTS (
         SELECT 1
     FROM dbo.Course c
-        INNER JOIN inserted i
-        ON c.code = i.code_course
+        INNER JOIN inserted i ON c.code = i.code_course
     )
     BEGIN
-        INSERT INTO dbo.Class
-            (
-            code,
-            title,
-            acronym,
-            description,
-            code_course)
-        SELECT
-            code,
-            title,
-            acronym,
-            description,
-            code_course
-        FROM inserted;
-    END
-    ELSE
-    BEGIN
-        RAISERROR ('Código do curso inválido.', 16, 1);
+        PRINT 'Código do curso inválido.';
         ROLLBACK;
         RETURN;
     END;
@@ -41,7 +22,7 @@ END;
 -- INSERT into dbo.Class
 --     (code, title, acronym, code_course)
 -- VALUES
---     ('23435', 'Matemática Aplicada', 'MATAP', 1)
+--     ('23435', 'Matemática Aplicada', 'MATAP', 110)
 
 -- delete from dbo.Class where code = '23435'
 
