fa8681c60bee3bac83f13bdab75202a998af45d5
CavasCallahan <dassuncao1234@gmail.com>
2023-06-02 22:14:42 +0100

    add: get view

--- logs/stored_procedures/sp_view_log.sql
+++ logs/stored_procedures/sp_view_log.sql
@@ -1,48 +1,49 @@
--- sp_view_log
-
 USE [db_index]
 go
 
--- Action
--- 0 - SELECT | 1 - INSERT | 2 - UPDATE | 3 DELETE | 4 - EXECUTE
--- N'{"table" : "Espadas", "action" : "EXECUTE", "columns" : []}'
-CREATE OR ALTER PROCEDURE sp_view_log
-	(
-	@info varchar(50)
-)
-AS
+DECLARE view_log CURSOR FOR
+SELECT
+	json_info,
+	createdAt,
+	deletedAt
+from dbo.log_action
+
+DECLARE
+	@jsonResult nvarchar(max),
+	@created_at date,
+	@deleted_at date
+
+OPEN view_log
+
+FETCH NEXT FROM view_log INTO 
+    @jsonResult,
+    @created_at,
+    @deleted_at
+
+WHILE @@FETCH_STATUS = 0
 BEGIN
 
-	declare @jsonResult nvarchar(max)
+	DECLARE @table varchar(50) = JSON_VALUE(@jsonResult, '$.table');
+	DECLARE @action varchar(50) = JSON_VALUE(@jsonResult, '$.action');
+	DECLARE @columns varchar(max) = JSON_QUERY(@jsonResult, '$.columns');
+	DECLARE @value nvarchar(max);
 
-	DECLARE @table varchar(50);
-	DECLARE @action varchar(50);
-	DECLARE @columns varchar(max);
-	DECLARE @created_at date;
-	DECLARE @deleted_at date;
+	PRINT ''
+	PRINT '-------------------'
+	PRINT 'Table: ' + @table
+	PRINT 'Action: ' + @action
+	PRINT '-------------------'
+	PRINT ''
+	-- PRINT 'Created At: ' + @created_at
+	-- PRINT 'Delted At' + @deleted_at
 
-	select
-		@jsonResult = json_info,
-		@created_at = createdAt,
-		@deleted_at = deletedAt
-	from dbo.log_action
-
-	SELECT @table = JSON_VALUE(@jsonResult, '$.table'),
-		@action = JSON_VALUE(@jsonResult, '$.action'),
-		@columns = JSON_QUERY(@jsonResult, '$.columns');
-
-	PRINT 'LOGS';
-	PRINT 'Table: ' + @table;
-	PRINT 'Action: ' + @action;
-	PRINT 'Columns: ' + ISNULL(@columns, '');
-	PRINT 'CREATED_AT: ' + CAST(@created_at as varchar);
-	PRINT 'DELETED_AT: ' + CAST(@deleted_at as varchar);
+	FETCH NEXT FROM view_log INTO 
+       	@jsonResult,
+		@created_at,
+		@deleted_at
 
 END
 GO
 
-exec dbo.sp_view_log 'table';
-go
-
-select *
-from dbo.log_action;
+CLOSE view_log
+DEALLOCATE view_log
