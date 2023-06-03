8634879fd295bd995fc20f857eb2f766c4c9b000
Davidbraga6 <davidjosebraga@gmail.com>
2023-06-03 23:58:19 +0100

    add: new two views with pivot

--- create_db/db_selects.sql
+++ create_db/db_selects.sql
@@ -1,5 +1,5 @@
 -- View de rank e dense rank das melhores médias em disciplinas no diferentes anos letivos (que aparecem do mais recente para o mais antigo) utiliza partition
-CREATE OR ALTER view rankMediasClass
+CREATE OR ALTER view rankAverageClass
 AS
     SELECT
         ci.[year],
@@ -7,16 +7,19 @@ AS
         c.acronym,
         RANK() OVER (PARTITION BY ci.[year] ORDER BY ci.value DESC) AS 'Rank',
         ci.[value],
-        DENSE_RANK() OVER (PARTITION BY ci.[year] ORDER BY ci.value DESC) AS 'Dense Rank'
+        DENSE_RANK() OVER (PARTITION BY ci.[year] ORDER BY ci.value DESC) AS 'Dense Rank',
+        ROW_NUMBER() OVER (PARTITION BY ci.[year] ORDER BY ci.value DESC) AS linha
     FROM dbo.Class_IndexType ci
         INNER JOIN dbo.Class c ON c.code = ci.code_class
     WHERE ci.[value] IS NOT NULL AND ci.id_index_type = 1
 GO
 
 -- SELECT *
--- FROM rankMediasClass
+-- FROM rankAverageClass
 -- ORDER BY [year] DESC;
 -- GO
+
+
 
 -- View para ver média dos cursos no ano letivo 2022/23, onde a média é superior a 9,5
 CREATE OR ALTER view showPositiveAverageCourses
@@ -42,6 +45,8 @@ GO
 -- ORDER BY AverageCourse DESC
 -- GO
 
+
+
 -- View para ver o numero de aprovados por exame dos cursos de todos os anos
 CREATE OR ALTER view shownStudentsPassedExam
 AS
@@ -57,11 +62,9 @@ AS
     GROUP BY co.code, co.title, co.acronym, ci.[year]
 GO
 
-
 -- SELECT *
 -- FROM shownStudentsPassedExam
 -- GO
-
 
 
 
@@ -79,7 +82,6 @@ AS
     WHERE ci.id_index_type = 7
     GROUP BY co.code, co.title, co.acronym, ci.[year]
 GO
-
 
 -- SELECT *
 -- FROM shownStudentsFailedExam
@@ -104,4 +106,55 @@ GO
 
 -- SELECT *
 -- FROM numResgisteredStudentsEI2022_23
+-- GO
+
+
+
+-- Pivot que mostra o número de alunos aprovados e reprovados
+CREATE OR ALTER view numStudentsAprovedAndDisapproved
+AS
+    SELECT *
+    FROM
+        (
+    SELECT co.title AS curso, co.acronym, i.title AS index_title, ci.value
+        FROM Course co
+            INNER JOIN Class c ON co.code = c.code_course
+            INNER JOIN Class_IndexType ci ON c.code = ci.code_class
+            INNER JOIN IndexType i ON ci.id_index_type = i.id
+) AS SourceTable
+PIVOT
+(
+    SUM(value) FOR index_title IN ([Número de Alunos Aprovados em Exame], [Número de Alunos Reprovados em Exame], [Número de Aprovados], [Número de Reprovados])
+) AS PivotTable
+GO
+
+-- SELECT * FROM numStudentsAprovedAndDisapproved
+-- GO
+
+
+
+-- Pivot que mostra o número de alunos aprovados em exame nos diferentes anos letivos
+CREATE OR ALTER view numStudentsAprovedByExam
+AS
+    SELECT *
+    FROM (
+SELECT
+            ci.[year], co.title,
+            SUM(ci.value) AS PassedExam
+        FROM dbo.Class_IndexType ci
+            INNER JOIN dbo.Class c
+            ON c.code = ci.code_class
+            INNER JOIN dbo.Course co
+            ON co.code = c.code_course
+        WHERE ci.id_index_type = 6
+        GROUP BY co.title, ci.[year]
+) AS SourceTable
+PIVOT
+(
+  SUM(PassedExam)
+  FOR year IN ([201617], [201718], [201819],[201920], [202021], [202122], [202223])
+) AS PivotTable;
+GO
+
+-- SELECT * FROM numStudentsAprovedByExam
 -- GO
